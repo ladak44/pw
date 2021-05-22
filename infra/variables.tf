5a6bd3653cfe63d3e77ab5ac639b5a00237b2f41
Marcin Ladak <marcin.ladak@outlook.com>
2021-05-22 12:23:28 +0200

    Dodano opisy i komentarze

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,27 +1,29 @@
+# Zmienne środowiskowe i tagi 
+# Nazwa regionu
 variable "region" {
    default = "us-east-1"
 }
-
+# Nazwa aplikacji
 variable "name" {
   default = "Airly"
 }
-
+# Nazwa projektu
 variable "project" {
   default = "PW"
 }
-
+# Nazwa środowiska w jakim utworzono projekt
 variable "environment" {
   default = "Big Data"
 }
-
+# Nazwa pary kluczy wykorzystywanej przy połączeniach SSH
 variable "key_name" {
   default = "aws_educate"
 }
-
+# Adress IP maszyny która będzie miała dostęp do klastra 
 variable "ingress_cidr_blocks" {
    default = "5.184.2.217/32"
 }
-
+# Nazwa bucketu w którym będą składowane dane
 variable "s3_bucket_name" {
   default = "airly-pw"
 }
