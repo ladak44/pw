9951b6d43b688fafda8fe1b431bb9e4fe7bf8216
Marcin Ladak <marcin.ladak@outlook.com>
2021-05-22 16:07:41 +0200

    Dodano opisy i komentarze

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,13 +1,13 @@
+# Profil dla roli EC2 w EMR
 resource "aws_iam_instance_profile" "emr_profile" {
-  name = "test_profile"
+  name = "EMR_EC2_Profile"
   role = aws_iam_role.role.name
 }
 
-
+# Rola EC2 
 resource "aws_iam_role" "role" {
-  name = "test_role"
+  name = "EMR_EC2_Role"
   path = "/"
-
   assume_role_policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -25,7 +25,7 @@ resource "aws_iam_role" "role" {
 EOF
 }
 
-# IAM Role for EMR cluster
+# Rola klastra EMR
 data "aws_iam_policy_document" "emr_assume_role" {
   statement {
     effect = "Allow"
@@ -38,18 +38,18 @@ data "aws_iam_policy_document" "emr_assume_role" {
     actions = ["sts:AssumeRole"]
   }
 }
-
+# Rola domyslna dla serwisu EMR
 resource "aws_iam_role" "emr_service_role" {
   name               = "EMR_ServiceRole"
   assume_role_policy = data.aws_iam_policy_document.emr_assume_role.json
 }
-
+# Polityka dla serwisu EMR
 resource "aws_iam_role_policy_attachment" "emr_service_role" {
   role       = aws_iam_role.emr_service_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceRole"
 }
 
-# IAM Role for autoscaling
+# Definicja polityki dla skalowania klastra EMR
 data "aws_iam_policy_document" "emr_autoscaling_role_policy" {
   statement {
     effect = "Allow"
@@ -62,12 +62,12 @@ data "aws_iam_policy_document" "emr_autoscaling_role_policy" {
     actions = ["sts:AssumeRole"]
   }
 }
-
+# Rola autoskalowania
 resource "aws_iam_role" "emr_autoscaling_role" {
   name               = "EMR_AutoScalingRole"
   assume_role_policy = data.aws_iam_policy_document.emr_autoscaling_role_policy.json
 }
-
+# Definicja polityki dla autoskalowania klastra
 resource "aws_iam_role_policy_attachment" "emr_autoscaling_role" {
   role       = aws_iam_role.emr_autoscaling_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceforAutoScalingRole"
